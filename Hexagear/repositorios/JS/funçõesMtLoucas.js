f62b94cb8ce2f0c6f594e9f9f26e313d8289b170
goitiueno <130381632+goitiueno@users.noreply.github.com>
2023-11-19 16:50:26 -0300

    Add files via upload

---
+++ Hexagear/repositorios/JS/funçõesMtLoucas.js
@@ -0,0 +1,18 @@
+function pistao(){
+    window.location.href="../../paginasWeb/pistao.html"
+}
+function biela(){
+    window.location.href="../../paginasWeb/biela.html"
+}
+function pinos(){
+    window.location.href="../../paginasWeb/pinos.html"
+}
+function valvulaAE(){
+    window.location.href="../../paginasWeb/valvulaAE.html"
+}
+function comando(){
+    window.location.href="../../paginasWeb/comando.html"
+}
+function bronzinas(){
+    window.location.href="../../paginasWeb/bronzinas.html"
+}
